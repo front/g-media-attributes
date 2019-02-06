0018ae27274a7bf05b2f46cfc99dc75b85188460
Elio Cro <elio.cro@gmail.com>
2019-02-06 14:39:04 +0000

    Override the core/image save/edit with higher order components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 
-import { blocks } from 'wp';
-const { registerBlockType, unregisterBlockType } = blocks;
+// import { blocks } from 'wp';
+// const { registerBlockType, unregisterBlockType } = blocks;
 
-import { name, settings } from './image';
+// import { name, settings } from './image';
 
 // setTimeout(() => {
-unregisterBlockType(name);
-registerBlockType(name, settings);
+// unregisterBlockType(name);
+// registerBlockType(name, settings);
 // });
+
+
+import overrideImage from './override';
+setTimeout(overrideImage);
+
---
+++ src/override.js
@@ -0,0 +1,96 @@
+
+import { blocks, element, apiFetch } from 'wp';
+
+const { registerBlockType, unregisterBlockType } = blocks;
+const { Component } = element;
+
+
+async function getMediaAttrs (id) {
+  const media = id && await apiFetch({ path: `/wp/v2/media/${id}` });
+
+  if(media && media.data) {
+    return Object.keys(media.data).reduce((d, k) => {
+      d[`data-${k.replace(/_/g, '-')}`] = media.data[k];
+      return d;
+    }, {});
+  }
+  return {};
+}
+
+
+export function editWithDataAttrs (EditComponent) {
+  return class extends Component {
+
+    componentDidMount () {
+      this.id = this.props.attributes && this.props.attributes.id;
+    }
+
+    async componentDidUpdate () {
+      const id = this.props.attributes && this.props.attributes.id;
+      if(id !== this.id) {
+        this.id = id;
+        const dataAttrs = await getMediaAttrs(this.id);
+        this.props.setAttributes({ dataAttrs });
+      }
+    }
+
+    render () {
+      return <EditComponent { ...this.props } />;
+    }
+  };
+}
+
+
+function updateTree (node, attrs) {
+  if(!node || !node.props) {
+    return;
+  }
+  const { type, props } = node;
+  if(type === 'img') {
+    node.props = {
+      ...props, ...attrs,
+    };
+  }
+
+  const { children } = props;
+  if(children && children.length) {
+    for(const item of children) {
+      updateTree(item, attrs);
+    }
+  }
+  else {
+    updateTree(children, attrs);
+  }
+}
+
+
+export function saveWithDataAttrs (saveFunc) {
+  return function (props) {
+    const dataAttrs = (props && props.attributes && props.attributes.dataAttrs) || {};
+    const tree = saveFunc(props);
+    updateTree(tree, dataAttrs);
+    return tree;
+  };
+}
+
+
+export default function () {
+  // Unregister and retrieve the image block
+  const image = unregisterBlockType('core/image');
+
+  // Add new attribute
+  image.attributes.dataAttrs = {
+    type: 'object',
+    default: {},
+  };
+
+  // Override save component
+  image.save = saveWithDataAttrs(image.save);
+
+  // Override edit component
+  image.edit = editWithDataAttrs(image.edit);
+
+
+  // Register the image again
+  registerBlockType('core/image', image);
+}
