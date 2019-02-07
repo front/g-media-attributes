38691156e289a927361e0e6336c2a0cd49ac7a4f
Elio Cro <elio.cro@gmail.com>
2019-02-07 16:09:38 +0000

    Fixed media attributes on gallery

--- src/gallery.js
+++ src/gallery.js
@@ -18,20 +18,13 @@ export function editWithMultipleMediaAttrs (EditComponent) {
       const ids = attributes && attributes.ids && attributes.ids.toString();
       if(ids !== this.ids) {
         this.ids = ids;
-        const { ids: newIds, mediaAttrs } = attributes;
+        const { ids: newIds, mediaAttrs: oldAttrs } = attributes;
+        const mediaAttrs = {};
 
         // Fetch the media attributes for new images
         for(const id of newIds) {
           if(id) {
-            mediaAttrs[id] = mediaAttrs[id] || await getMediaAttrs(id);
-          }
-        }
-
-        // Remove the media attributes for old images
-        const oldIds = Object.keys(mediaAttrs).map(i => parseInt(i) || 0);
-        for(const id of oldIds) {
-          if(id && !newIds.includes(id)) {
-            delete mediaAttrs[id];
+            mediaAttrs[id] = oldAttrs[id] || await getMediaAttrs(id);
           }
         }
 
