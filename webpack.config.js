88cfb68599011efcfbef00e70cdb3b393f7dbc7c
Elio Cro <elio.cro@gmail.com>
2019-02-07 10:56:35 +0000

    Simplify webpack and dependencies setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const cleanBuild = new CleanWebpackPlugin(['build']);
-const extractCSS = new ExtractTextPlugin('style.css');
 
 module.exports = {
   entry: './src/index.js',
@@ -23,20 +21,10 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
-      {
-        test: /\.s?css$/,
-        exclude: /node_modules/,
-        use: extractCSS.extract(['css-loader', 'sass-loader']),
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        loader: 'url-loader',
-      },
     ],
   },
   mode: 'production',
   plugins: [
     cleanBuild,
-    extractCSS,
   ],
 };
